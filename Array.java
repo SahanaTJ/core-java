bed9e1b1294aa59ff3264064a383dee8f71b250a
SahanaTJ <113516386+SahanaTJ@users.noreply.github.com>
2022-09-22 00:59:11 -0700

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,14 @@
+class Array
+{
+public static void main(String a[])
+{  
+									  // double can hold byte,short,int, long,double,float
+									  //Long can hold byte,short,int,long
+double r[]={457L,80,90,90,00};
+for (int z=0; z<r.length;z++){
+System.out.println("data in the index"+ z +" "+  r[z]);
+}
+
+}
+
+}
