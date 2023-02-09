45dafaa1c3352ba51f2dcf97688d8cc5dce72f6c
SahanaTJ <sahanatj.xworkz@gmail.com>
2023-02-09 15:38:59 -0800

    beaches

---
+++ Beaches/src/com/xworkz/beachapp/Beaches.java
@@ -0,0 +1,18 @@
+package com.xworkz.beachapp;
+
+//data transfer object  8 dto is must everything is uppercase --Design pattren
+public class BeachesDTO {
+  
+	private int id;
+	private String name;
+	
+	public int getId() {
+		return id;
+		}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+}
+ //java understand class and objects
+//
